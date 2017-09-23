43876e8d924446066ec91dea5b7d2cb6138459a6
krsrv <krsrv@cse.iitb.ac.in>
2017-09-24 02:27:58 +0530

    Viewing directories on seperate webpages

---
+++ share/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'share'
+urlpatterns = [
+    url(r'^(?P<addr>.*)$', views.directory, name = 'addr') #parse as complete address in the url 
+]
--- share/views.py
+++ share/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import os
+
+def directory(request, addr):
+    if not addr or addr[0] != '/':
+        home = os.path.expanduser('~') + '/' 
+    else:
+        home = ''
+
+    directory_list = [x + os.path.expanduser('/') for x in os.listdir(home + addr)]
+    return render(request, 'share/directory.html', {
+            'directory' : home + addr,
+            'directory_list' : directory_list
+            })
 
 # Create your views here.
