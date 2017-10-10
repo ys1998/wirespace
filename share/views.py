be4a4f840611e70da2114a5c3ea1a1cbc029cb67
krsrv <krsrv@cse.iitb.ac.in>
2017-10-10 16:11:21 +0530

    Removed swap files

--- share/views.py
+++ share/views.py
@@ -145,6 +145,7 @@ def download_item(request):
 		else:
 			return get_file(target, "download")
 
+@csrf_exempt
 def upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
